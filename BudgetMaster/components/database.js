a612a8bfe8d3df9fa4128b17dc2d2ed480416574
fechetejSPU <121974712+fechetejSPU@users.noreply.github.com>
2023-06-03 22:54:27 -0700

    Made budget methods

--- BudgetMaster/components/database.js
+++ BudgetMaster/components/database.js
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import * as SQLite from "expo-sqlite";
 
 export default function Database() {
@@ -107,6 +106,81 @@ export default function Database() {
                     }
                 );
             });
+        });
+    }
+
+    //not sure where is best to document this, but gonna assume that month integer is months since 2020, so (curYear-2020)*12+curMonth
+    this.getBudgets = (month) => {
+        return new Promise((resolve, reject) => {
+            db.transaction((tx) => {
+                tx.executeSql(
+                    "SELECT ID FROM Budget WHERE Month=?",
+                    [month],
+                    (_tx, result) => {
+                        let ids = [];
+                        for (let i = 0; i < result.rows.length; i++) {
+                            ids.push(result.rows.item(i).ID);
+                        }
+                        resolve(ids);
+                    },
+                    (_tx, err) => {
+                        reject(err);
+                    }
+                );
+            });
+        });
+    }
+
+    this.getBudget = (id) => {
+        return new Promise((resolve, reject) => {
+            db.transaction((tx) => {
+                tx.executeSql(
+                    "SELECT Amount, CatName, Month FROM Budget WHERE ID=?",
+                    [id],
+                    (_tx, result) => {
+                        resolve(result.rows.item(0));
+                    },
+                    (_tx, err) => {
+                        reject(err);
+                    }
+                );
+            });
+        });
+    }
+
+    //not on this since shouldn't be called by other code
+    //all budgets should be auto-created as expenses are added
+    getCatBudget = (catId, month) => {
+        return new Promise((resolve, reject) => {
+            this.getCategory(catId).then((cat) => {
+                db.transaction((tx) => {
+                    tx.executeSql(
+                        "SELECT ID from Budget WHERE CatName=? AND Month=?",
+                        [cat.CatName, month],
+                        (_tx, result) => {
+                            if (result.rows.length == 0) {
+                                tx.executeSql(
+                                    "INSERT INTO Budget (CatName, Amount, Month) VALUES (?, ?, ?)",
+                                    [cat.CatName, cat.Amount, month],
+                                    (_tx, _) => {
+                                        getCatBudget(catId, month).then((budgetId) => {
+                                            resolve(budgetId);
+                                        });
+                                    },
+                                    (_tx, err) => {
+                                        reject(err);
+                                    }
+                                );
+                            } else {
+                                resolve(result.rows.item(0));
+                            }
+                        },
+                        (_tx, err) => {
+                            reject(err);
+                        }
+                    );
+                });
+            })
         });
     }
 
