4cbc83ca509f8c9becf70e85eb7f412b134b9b45
fechetejSPU <121974712+fechetejSPU@users.noreply.github.com>
2023-06-06 00:09:56 -0700

    Made draggable input

--- BudgetMaster/components/spending.js
+++ BudgetMaster/components/spending.js
@@ -1,13 +1,28 @@
-import {View, Text, Modal} from "react-native";
+import {View, Text, Modal, TextInput} from "react-native";
 import Navigation from './navigation';
+import Draggable from "react-native-draggable";
+import {useState} from "react";
 
 export default function Spending(props) {
+    const [getSpending, setSpending] = useState("0");
+
     return (<Modal visible={props.getMode == "spending"}>
         <View>
             <Navigation
                 setMode={props.setMode}
             />
             <Text>Spending</Text>
+            <View>
+                <Draggable
+                    shouldReverse
+                >
+                    <TextInput
+                        inputMode="numeric"
+                        onChangeText={setSpending}
+                        value={getSpending}
+                    />
+                </Draggable>
+            </View>
         </View>
     </Modal>);
 }
